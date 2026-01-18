3b945f4f642273a1d14df691684ffdf88d79501f
Michael Green <m_green@hotmail.co.nz>
2026-01-18 20:38:44 +1100

    feat: add favicon

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from .routes import auth, chat
 
 app = FastAPI()
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("src/static/favicon.ico")
 
 app.include_router(auth.router)
 app.include_router(chat.router)
